aeb89e335a390310ede604998298177dc30114f4
YuzukiTsuru <740291272@qq.com>
2020-08-24 19:58:03 +0800

    [ADD][Feature] MusicXML2UST

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from MusicXMLPaser.MusicXMLPaser import MusicXMLDocument
 from USTPaser.USTPaser import USTPaser
 
+import argparse
 
-def main():
+
+def main(path):
     tempos = 120
-    score_parser = MusicXMLDocument('test/01.xml')
+    score_parser = MusicXMLDocument(path)
     for tempo in score_parser.get_tempos():
         tempos = tempo.qpm
-        print(tempos)
 
     xml_data = []
     i = 0
@@ -17,8 +18,21 @@ def main():
                 xml_data.insert(i, note.get_note())
                 i += i
 
-    print(USTPaser(xml_data, tempos).get_ust())
+    return USTPaser(xml_data, tempos).get_ust()
 
 
 if __name__ == '__main__':
-    pass
+    import sys
+    if len(sys.argv) < 1:
+        print('MusicXML To UST Ver 0.1 \nusage: main [-h] [-i INPUT]')
+    else:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', '--input', default=None, type=str, help='path to MusicXML (default: None)')
+        # TODO: 文件夹下全部文件转换
+        # parser.add_argument('-d', '--dir', default=None, type=str, help='path to MusicXML directory (default: None)')
+
+        args = parser.parse_args()
+        with open(args.input[0: len(args.input) - 3] + 'ust', mode='w') as f:
+            f.write(main(args.input))
+
+        print('Conversion Complete, File: ' + args.input)
